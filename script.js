34d1c4c5c38c9390895573a93bc8b120c00ea176
ddmeyer23 <78511312+ddmeyer23@users.noreply.github.com>
2021-02-24 00:12:49 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+require(["esri/config","esri/Map", "esri/views/MapView","esri/widgets/BasemapToggle", "esri/widgets/BasemapGallery", "esri/layers/FeatureLayer", "esri/WebMap"], 
+      
+        function (esriConfig,Map, MapView,BasemapToggle, BasemapGallery, FeatureLayer, WebMap){
+  
+  var webmap = new WebMap({
+    portalItem: {// autocasts new PortalItem()
+      id: "a45fe02721f84bf58f28e74c9771bfc5"
+    }
+  });
+  
+  //setting the webmap instance to the map property in a mapview//
+  var view = new MapView({
+    map: webmap,
+    container: "viewDiv",
+    zoom: 9,
+    center: [-88.7879,43.7844]
+    
+  });
+});
